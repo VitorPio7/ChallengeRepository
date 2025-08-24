22c6c6dd2ae01f6c461722760ce793928fb75a72
VitorPio7 <vitorvpio60@gmail.com>
2025-08-23 21:04:40 -0300

    feat:finish backend

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,23 +1,63 @@
 import Task from "../models/taskModel.js";
+import AppError from "../utils/appError.js";
+import catchAsync from "../utils/catchAsync.js";
 
-export const getTasks = async (req, res)=>{
+export const getTasks = catchAsync(async (req, res)=>{
     const tasks = await Task.find();
-    res.json(tasks);
-}
+    if (!tasks) {
+        return next((new AppError('Não existe tarefas!', 404)))
+    }
+    res.status(200).json({
+        status: 'Tasks adquiridas com sucesso!',
+        results: tasks.length,
+        data: {
+            tasks
+        }
+    });
+});
 
-export const createTask = async (req, res) =>{
+export const getTask = catchAsync(async (req, res, next)=> {
+    const task = await Task.findById(req.params.id);
+    if(!task){
+        return next(new AppError('Nenhuma tarefa encontrada com essa informação', 404));
+    }
+    res.status(200).json({
+        status: 'Encontramos a sua tarefa!',
+        data: {
+         task
+        }
+    })
+})
+export const createTask = catchAsync(async (req, res) =>{
     const { title, description} = req.body;
     const task = new Task({title, description});
     await task.save();
-    res.status(201).json(task);
-}
+    res.status(201).json({
+        status: 'success',
+        results: task.length,
+        data:{
+            task
+        }
+    });
+})
 export const updateTask = async(req, res) =>{
     const {id} = req.params;
     const updateTask = await Task.findByIdAndUpdate(id, req.body, {new: true});
-    res.json(updateTask)
+    if(!updateTask){
+        return next(new AppError("Nenhum dado encontrado com essa ID", 404));
+    }
+    res.status(200).json({
+        status: 'Atualização da task realizada com sucesso!',
+        data: {
+            updateTask
+        }
+    })
 }
 export const deleteTask = async (req, res) =>{
     const {id} = req.params;
-    await Task.findByIdAndDelete(id);
-    res.json({message: "Tarefa removida"});
+    const deletarTask = await Task.findByIdAndDelete(id);
+    if(!deletarTask) {
+       return next(new AppError('Nenhuma task encontrada com esse ID', 404)) 
+    }
+    res.status(204).json({message: "Tarefa removida"});
 }
--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,9 +1,29 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-    title:{type:Boolean, required:true},
-    description: String,
-    completed: {type: Boolean, default:false},
+    titulo: { 
+        type:String, 
+        required:[true, "É necessário colocar o titulo da tarefa."], 
+        trim: true,
+        maxLength: [70, "É necessário colocar no máximo 70 caracteres."],
+        minLength: [2, "É necessário colocar no mínimo 2 caracteres."]
+    },
+    inicio: {
+        type:Date, 
+        required:true
+    },
+    conclusao: {
+        type:Date, 
+        required:[true, "Você precisa passar um data de conclusão"]
+    },
+    custoEstimado: {
+        type:Boolean, 
+        required:[true, "Você precisa passar um custo estimado."]
+    },
+    statusTarefa:{
+        type:String, 
+        require:[true, "Você precisa passar o status da sua tarefa."]
+    }
 },{timestamps:true});
 
 export default mongoose.model("Task", taskSchema);
