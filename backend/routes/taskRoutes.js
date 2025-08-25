af68327a804e04fbbc4c9b7a7c0ff66f6d611495
VitorPio7 <vitorvpio60@gmail.com>
2025-08-25 13:24:30 -0300

    feat:check the nosql injection

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,56 @@
 import express from 'express';
 import { getTasks, createTask, updateTask, deleteTask, getTask } from '../controllers/taskController.js';
-
+import { validationResult, body, param } from "express-validator";
 const router = express.Router();
 
+const validaet = (req,res,next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    next()
+}
+
 router.get("/", getTasks);
-router.get("/:id", getTask);
-router.post("/", createTask);
-router.put("/:id", updateTask);
-router.delete("/:id",deleteTask);
+
+router.get("/:id", 
+    [ 
+    param("id").isMongoId().withMessage("ID inválido")
+    ],
+    validaet,
+    getTask
+);
+
+router.post("/",
+    [
+      body("titulo").notEmpty().isString(),
+      body("inicio").notEmpty().isISO8601().withMessage("Data inválida"),
+      body("conclusao").notEmpty().isISO8601().withMessage("Data inválida"),
+      body("custoEstimado").notEmpty().isNumeric(),
+      body("statusTarefa").notEmpty().isString()  
+    ], 
+    validaet ,
+    createTask
+);
+
+router.put("/:id",
+    [
+     param("id").isMongoId().withMessage("ID inválido"),
+     body("titulo").notEmpty().isString(),
+     body("inicio").notEmpty().isISO6391(),
+     body("conclusao").notEmpty().isString(),
+     body("custoEstimado").notEmpty().isNumeric(),
+     body("statusTarefa").notEmpty().isString()
+    ],
+    validaet,
+    updateTask
+);
+
+router.delete("/:id",
+     [
+      param("id").isMongoId().withMessage("ID inválido")
+     ],
+    validaet,
+    deleteTask);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,40 @@ import express from 'express';
 import cors from "cors";
 import mongoose from 'mongoose';
 import taskRoutes from './routes/taskRoutes.js';
-import AppError from './utils/appError.js';
+import helmet from "helmet";
 import globalErrorHandler from './controllers/errorController.js'
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
+import dotenv from 'dotenv'
 
+dotenv.config({path:'./.env'});
 const app = express();
-const porta = 5000;
+const porta = process.env.PORT || 5000;
+const limitador = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message:"você realizou varios requests nesse IP, favor esperar 15 minutos."   
+})
+app.use(mongoSanitize());
+
+app.use((req, res, next)=>{
+    res.status(404).json({
+        erro: "Rota não encontrada.",
+        caminho: req.originalUrl
+    })
+});
+
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).json({
+        erro: "Erro interno no servidor."
+    })
+})
+app.use(limitador);
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect("mongodb://127.0.0.1:27017/tarefas");
-
+app.use(helmet())
+mongoose.connect(process.env.DB);
 app.use("/api/tasks",taskRoutes);
 app.use(globalErrorHandler);
 app.listen(porta, ()=> console.log(`Servidor rodando na porta ${porta}.`))
